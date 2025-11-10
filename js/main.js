f997bdfa0cf5e017e309e39ad2e4fee4fe38298c
Monica Coca <mcocapelaz@gmail.com>
2025-11-10 13:55:48 +0100

    fase/6 completed

--- js/main.js
+++ js/main.js
@@ -6,24 +6,25 @@
 
 // VARIABLES
 
-const myWordList =  [
+const myWordList = [
   {
-    text: 'Pencil',
-    total: 6
+    text: "Pencil",
+    total: 6,
   },
   {
-    text: 'Thermo',
-    total: 2
+    text: "Thermo",
+    total: 2,
   },
   {
-    text: 'TV',
-    total: 8
+    text: "TV",
+    total: 8,
   },
   {
-    text: 'Phone',
-    total: 4
-  }
+    text: "Phone",
+    total: 4,
+  },
 ];
+
 
 // FUNCIONES
 
@@ -41,21 +42,29 @@ writeThis("Patata", getRandomNumber(10));
 writeThis("Aguacate", getRandomNumber(10));
 writeThis("Pizza", getRandomNumber(10));
 
-function writeMyArray () {
-    for (let i = 0; i < myWordList.length; i++) 
-        writeThis(myWordList[i].text,myWordList[i].total);       
+function writeMyArray() {
+  for (let i = 0; i < myWordList.length; i++)
+    writeThis(myWordList[i].text, myWordList[i].total);
 }
 
-writeMyArray () 
+writeMyArray();
 
 // EVENTOS
 
 //CÓDIGO AL CARGAR LA PÁGINA
 
 
-fetch(
-  "https://beta.adalab.es/ejercicios-extra/js-funciones-y-parametros-desde-cero/data.json"
-)
+fetch("https://beta.adalab.es/ejercicios-extra/js-funciones-y-parametros-desde-cero/data.json")
   .then((response) => response.json())
-  .then((data) => console.log(data))
-  .catch((error) => console.error("Error:", error));
+  .then((data) => {
+    console.log("Datos recibidos:", data);
+    
+    for (let i = 0; i < data.length; i++) {
+      writeThis(data[i].text, data[i].total);
+    }
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+  });
+
+
