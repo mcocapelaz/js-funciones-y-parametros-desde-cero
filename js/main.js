9708b2a57b27be11a35382b32f88f89395e28a81
Monica Coca <mcocapelaz@gmail.com>
2025-11-10 13:12:50 +0100

    fase5 completed

--- js/main.js
+++ js/main.js
@@ -6,6 +6,25 @@
 
 // VARIABLES
 
+const myWordList =  [
+  {
+    text: 'Pencil',
+    total: 6
+  },
+  {
+    text: 'Thermo',
+    total: 2
+  },
+  {
+    text: 'TV',
+    total: 8
+  },
+  {
+    text: 'Phone',
+    total: 4
+  }
+];
+
 // FUNCIONES
 
 function writeThis(word, number) {
@@ -21,6 +40,16 @@ function getRandomNumber(max) {
 writeThis("Patata", getRandomNumber(10));
 writeThis("Aguacate", getRandomNumber(10));
 writeThis("Pizza", getRandomNumber(10));
+
+function writeMyArray () {
+    for (let i = 0; i < myWordList.length; i++) 
+        writeThis(myWordList[i].text,myWordList[i].total);       
+}
+
+writeMyArray () 
+
+
+
 
 // EVENTOS
 
