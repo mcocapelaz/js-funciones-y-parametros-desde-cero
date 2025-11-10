51ef783cdb652af643d1be67664333d1b0cf3985
Monica Coca <mcocapelaz@gmail.com>
2025-11-10 11:39:53 +0100

    "fase 1 completed"

---
+++ js/main.js
@@ -0,0 +1,37 @@
+"use strict";
+
+// QUERY SELECTOR 
+
+
+
+// DATOS 
+
+
+
+
+
+// VARIABLES 
+
+
+
+// FUNCIONES 
+
+function ten() {
+for (let i=0; i<10; i++) {
+    console.log('Patata')
+}
+}
+
+ten()
+
+
+
+
+// EVENTOS 
+
+
+//CÓDIGO AL CARGAR LA PÁGINA 
+
+
+
+
