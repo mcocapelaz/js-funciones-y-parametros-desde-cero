0521a1abcae182a68591a07b155f9434266acd93
Monica Coca <mcocapelaz@gmail.com>
2025-11-10 13:29:16 +0100

    fetch first code in fase/6

--- js/main.js
+++ js/main.js
@@ -6,6 +6,25 @@
 
 // VARIABLES
 
+const myWordList =  [
+  {
+    text: 'Pencil',
+    total: 6
+  },
+  {
+    text: 'Thermo',
+    total: 2
+  },
+  {
+    text: 'TV',
+    total: 8
+  },
+  {
+    text: 'Phone',
+    total: 4
+  }
+];
+
 // FUNCIONES
 
 function writeThis(word, number) {
@@ -22,6 +41,21 @@ writeThis("Patata", getRandomNumber(10));
 writeThis("Aguacate", getRandomNumber(10));
 writeThis("Pizza", getRandomNumber(10));
 
+function writeMyArray () {
+    for (let i = 0; i < myWordList.length; i++) 
+        writeThis(myWordList[i].text,myWordList[i].total);       
+}
+
+writeMyArray () 
+
 // EVENTOS
 
 //CÓDIGO AL CARGAR LA PÁGINA
+
+
+fetch(
+  "https://beta.adalab.es/ejercicios-extra/js-funciones-y-parametros-desde-cero/data.json"
+)
+  .then((response) => response.json())
+  .then((data) => console.log(data))
+  .catch((error) => console.error("Error:", error));
